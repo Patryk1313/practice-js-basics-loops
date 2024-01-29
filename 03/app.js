65146a7aacd879e1ff279219a0da07a91f6fc60a
Patryk <patryk.13@o2.pl>
2024-01-29 12:23:30 +0100

    Done tasks

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,17 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+let i = 0;
+
+while (i <= x) {
+    
+    if(i == randomNumber) {
+        console.log(iteration);
+    } else {
+        console.log("Nie znaleziono liczby ");
+    }
+
+    iteration++
+    i++
+}
